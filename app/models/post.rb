89783c23d3fa1559c8d9761a1ad549221de8cc71
kihno <stistyle13@gmail.com>
2023-05-22 15:28:43 -0700

    Add devise for user registration and log in

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 500 }
+
+  has_many_attached :images
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    get '/users', to: 'devise/registration#new'
+    get 'users/password', to: 'devise/passwords#new'
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
