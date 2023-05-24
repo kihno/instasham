e698b1eccc1cb9fb494dcb2b1d88dc2079bcda04
kihno <stistyle13@gmail.com>
2023-05-24 15:01:03 -0700

    Make comments likeable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_24_005651) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_24_194012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,10 +44,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_24_005651) do
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
-    t.string "user_id"
     t.string "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -86,5 +87,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_24_005651) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "users"
 end
